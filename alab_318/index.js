aecffab7f727b0e0cbcf87df09cff20029f07a41
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-01-18 10:36:42 -0500

    first commit

---
+++ alab_318/index.js
@@ -0,0 +1,11 @@
+const express = require(`express`);
+const app = express();
+const cors = require("cors");
+
+app.set(`view engine`, `ejs`);
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+const userRouter= require(`./routes/users`);
+app.use(`/users`, userRouter)
+app.listen(8080);
